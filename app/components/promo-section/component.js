0708261a921d9821fdf3f07321f437ea423cfeb3
Jason Barry <barryofguilder@gmail.com>
2016-12-21 21:47:40 -0500

    Add Christmas hours

--- app/components/promo-section/component.js
+++ app/components/promo-section/component.js
@@ -3,6 +3,8 @@ import Ember from 'ember';
 export default Ember.Component.extend({
   tagName: 'section',
   classNames: ['butcher-promo'],
+  classNameBindings: ['specialHours:special-hours'],
 
+  specialHours: false,
   darken: false
 });
