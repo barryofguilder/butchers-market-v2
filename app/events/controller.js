23a14a78b5d575b1fec0bc87a7eed7436a293ca1
Jason Barry <barryofguilder@gmail.com>
2018-01-13 11:22:31 -0500

    Make past performances load from a data file

--- app/events/controller.js
+++ app/events/controller.js
@@ -1,7 +1,30 @@
 import Controller from '@ember/controller';
 import MediaMixin from '../mixins/media-mixin';
+import { computed } from '@ember/object';
+
+const PERFORMANCES_TO_SHOW = 5;
 
 export default Controller.extend(MediaMixin, {
   queryParams: ['events'],
-  events: false
+  events: false,
+
+  filteredPerformances: computed('model.performances', function() {
+    let performances = this.get('model.performances');
+    let performanceCount = performances.get('length');
+    let randomPerformances = [];
+
+    while (randomPerformances.get('length') < performanceCount) {
+      let performance = performances.objectAt(Math.floor(Math.random() * performanceCount));
+
+      let found = randomPerformances.findBy('id', performance.get('id'));
+
+      if (found) {
+        continue;
+      }
+
+      randomPerformances.pushObject(performance);
+    }
+
+    return randomPerformances.slice(0, PERFORMANCES_TO_SHOW);
+  })
 });
--- app/events/route.js
+++ app/events/route.js
@@ -13,6 +13,7 @@ export default Route.extend(ResetScrollMixin, MobileAppMixin, {
   model() {
     return RSVP.hash({
       events: this.store.findAll('event'),
+      performances: this.store.findAll('performance'),
       reviews: this.store.findAll('review')
     });
   },
