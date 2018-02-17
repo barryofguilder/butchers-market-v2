2fb4231240d7c9f07dd75c06fea5d18e0efd8f56
Jason Barry <barryofguilder@gmail.com>
2018-02-16 20:49:32 -0500

    Add the taco countdown to the main page (#21)

--- app/index/controller.js
+++ app/index/controller.js
@@ -1,7 +1,32 @@
 import { filterBy } from '@ember/object/computed';
 import Controller from '@ember/controller';
 import MediaMixin from 'butchers-market/mixins/media-mixin';
+import { computed } from '@ember/object';
+import moment from 'moment';
 
 export default Controller.extend(MediaMixin, {
-  featuredBundles: filterBy('model.bundles', 'featured', true)
+  featuredBundles: filterBy('model.bundles', 'featured', true),
+
+  tacoSeasonDate: null,
+  showTacoSeason: computed('tacoSeasonDate', function() {
+    let takedownDate = moment(this.get('tacoSeasonDate')).add(7, 'days');
+    let now = moment();
+
+    return now.isBefore(takedownDate);
+  }),
+  tacoSeasonArrived: computed('tacoSeasonDate', function() {
+    let now = moment();
+
+    return now.isAfter(this.get('tacoSeasonDate'), 'second');
+  }),
+  tacoSeasonClass: computed('showTacoSeason', function() {
+    return this.get('showTacoSeason') ? 'taco-season' : null;
+  }),
+
+  init() {
+    this._super(...arguments);
+
+    // March 20, 2018 9:00am
+    this.set('tacoSeasonDate', new Date(2018, 2, 20, 10, 0));
+  }
 });
