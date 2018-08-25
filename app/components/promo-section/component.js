a11b0189f8f7285f0484a7b67f4be2444a8152f8
Jason Barry <barryofguilder@gmail.com>
2018-08-25 15:43:06 -0400

    Update the deli promo pic

--- app/components/promo-section/component.js
+++ app/components/promo-section/component.js
@@ -3,7 +3,7 @@ import Component from '@ember/component';
 export default Component.extend({
   tagName: 'section',
   classNames: ['butcher-promo'],
-  classNameBindings: ['specialHours:special-hours'],
+  classNameBindings: ['specialHours:special-hours', 'darken:darken'],
 
   specialHours: false,
   darken: false
