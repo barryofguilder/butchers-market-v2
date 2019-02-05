6a6c50a39d61c79bc06aca2bf8bb0d921a2edd43
Jason Barry <barryofguilder@gmail.com>
2019-02-04 22:15:21 -0500

    Update carousel

--- ember-cli-build.js
+++ ember-cli-build.js
@@ -4,9 +4,6 @@ const EmberApp = require('ember-cli/lib/broccoli/ember-app');
 
 module.exports = function(defaults) {
   let app = new EmberApp(defaults, {
-    'ember-font-awesome': {
-      useScss: true
-    },
     fingerprint: {
       exclude: ['apple-touch-icon.png', 'images/*.*']
     }
@@ -29,10 +26,10 @@ module.exports = function(defaults) {
   //app.import('node_modules/popper.js/dist/umd/popper.js');
 
   // Bootstrap
-  //app.import('node_modules/bootstrap/js/dist/util.js');
+  app.import('node_modules/bootstrap/js/dist/util.js');
   //app.import('bower_components/bootstrap/js/dist/alert.js');
   //app.import('node_modules/bootstrap/js/dist/button.js');
-  //app.import('node_modules/bootstrap/js/dist/carousel.js');
+  app.import('node_modules/bootstrap/js/dist/carousel.js');
   //app.import('node_modules/bootstrap/js/dist/collapse.js');
   //app.import('node_modules/bootstrap/js/dist/dropdown.js');
   //app.import('node_modules/bootstrap/js/dist/modal.js');
