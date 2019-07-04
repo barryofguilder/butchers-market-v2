7a367f26c562fe84ba40a39187b78d377a2e4e85
Jason Barry <barryofguilder@gmail.com>
2019-07-03 23:20:14 -0400

    Add Tailwind CSS and remove SASS and Bootstrap

---
+++ app/tailwind/config.js
@@ -0,0 +1,8 @@
+/* eslint-disable-next-line */
+module.exports = {
+  theme: {
+    extend: {},
+  },
+  variants: {},
+  plugins: [],
+};
--- ember-cli-build.js
+++ ember-cli-build.js
@@ -7,36 +7,13 @@ module.exports = function(defaults) {
     fingerprint: {
       exclude: ['apple-touch-icon.png', 'images/*.*'],
     },
+
+    postcssOptions: {
+      compile: {
+        plugins: [require('tailwindcss')('app/tailwind/config.js')],
+      },
+    },
   });
-
-  // Use `app.import` to add additional libraries to the generated
-  // output files.
-  //
-  // If you need to use different assets in different
-  // environments, specify an object as the first parameter. That
-  // object's keys should be the environment name and the values
-  // should be the asset to use in that environment.
-  //
-  // If the library that you are including contains AMD or ES6
-  // modules that you would like to import into your application
-  // please specify an object with the list of modules as keys
-  // along with the exports of each module as its value.
-
-  // Bootstrap dependency for dropdowns
-  app.import('node_modules/popper.js/dist/umd/popper.js');
-
-  // Bootstrap
-  app.import('node_modules/bootstrap/js/dist/util.js');
-  //app.import('bower_components/bootstrap/js/dist/alert.js');
-  app.import('node_modules/bootstrap/js/dist/button.js');
-  app.import('node_modules/bootstrap/js/dist/carousel.js');
-  app.import('node_modules/bootstrap/js/dist/collapse.js');
-  app.import('node_modules/bootstrap/js/dist/dropdown.js');
-  //app.import('node_modules/bootstrap/js/dist/modal.js');
-  //app.import('node_modules/bootstrap/js/dist/tooltip.js');
-  //app.import('node_modules/bootstrap/js/dist/popover.js');
-  //app.import('node_modules/bootstrap/js/dist/scrollspy.js');
-  //app.import('node_modules/bootstrap/js/dist/tab.js');
 
   return app.toTree();
 };
