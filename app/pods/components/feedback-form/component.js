f28ab107dad697ecd8c37e6db2ceacf27489f570
Jason Barry <barryofguilder@gmail.com>
2019-10-23 22:00:25 -0400

    Update ajax with fetch

--- app/pods/components/feedback-form/component.js
+++ app/pods/components/feedback-form/component.js
@@ -1,12 +1,11 @@
 import Component from '@ember/component';
-import { inject as service } from '@ember/service';
 import { computed } from '@ember/object';
 import config from 'butchers-market/config/environment';
+import { task } from 'ember-concurrency';
+import fetch from 'fetch';
 
 export default Component.extend({
   tagName: '',
-
-  ajax: service(),
 
   showReCaptcha: config.showReCaptcha,
   name: null,
@@ -34,70 +33,69 @@ export default Component.extend({
     }
   ),
 
+  sendFeedbackTask: task(function*() {
+    let payload = {
+      method: 'POST',
+      body: JSON.stringify({
+        name: this.get('name'),
+        email: this.get('email'),
+        message: this.get('message'),
+        recaptcha: this.get('recaptcha'),
+      }),
+    };
+
+    const response = yield fetch('/server/feedback.php', payload);
+    const json = yield response.json();
+
+    if (response.status >= 200 && response.status < 300) {
+      this.setProperties({
+        feedbackSent: true,
+        nameInvalid: false,
+        emailInvalid: false,
+        messageInvalid: false,
+        recaptchaInvalid: false,
+        showServerError: false,
+      });
+    } else {
+      if (json && json.errors && json.errors.length > 0) {
+        let error = json.errors[0];
+
+        if (error.detail.name) {
+          this.set('nameInvalid', true);
+        } else {
+          this.set('nameInvalid', false);
+        }
+
+        if (error.detail.email) {
+          this.set('emailInvalid', true);
+        } else {
+          this.set('emailInvalid', false);
+        }
+
+        if (error.detail.message) {
+          this.set('messageInvalid', true);
+        } else {
+          this.set('messageInvalid', false);
+        }
+
+        if (error.detail.recaptcha) {
+          this.set('recaptchaInvalid', true);
+        } else {
+          this.set('recaptchaInvalid', false);
+        }
+      } else {
+        this.set('showServerError', true);
+      }
+    }
+  }).drop(),
+
   actions: {
     onCaptchaResolved(reCaptchaResponse) {
       this.set('recaptcha', reCaptchaResponse);
     },
 
     sendFeedback() {
-      let payload = {
-        data: JSON.stringify({
-          name: this.get('name'),
-          email: this.get('email'),
-          message: this.get('message'),
-          recaptcha: this.get('recaptcha'),
-        }),
-        contentType: 'application/json',
-      };
-
-      this.get('ajax')
-        .post('/server/feedback.php', payload)
-        .then(() => {
-          this.setProperties({
-            feedbackSent: true,
-            nameInvalid: false,
-            emailInvalid: false,
-            messageInvalid: false,
-            recaptchaInvalid: false,
-            showServerError: false,
-          });
-        })
-        .catch(response => {
-          if (
-            response &&
-            response.payload &&
-            response.payload.errors &&
-            response.payload.errors.length > 0
-          ) {
-            let error = response.payload.errors[0];
-
-            if (error.detail.name) {
-              this.set('nameInvalid', true);
-            } else {
-              this.set('nameInvalid', false);
-            }
-
-            if (error.detail.email) {
-              this.set('emailInvalid', true);
-            } else {
-              this.set('emailInvalid', false);
-            }
-
-            if (error.detail.message) {
-              this.set('messageInvalid', true);
-            } else {
-              this.set('messageInvalid', false);
-            }
-
-            if (error.detail.recaptcha) {
-              this.set('recaptchaInvalid', true);
-            } else {
-              this.set('recaptchaInvalid', false);
-            }
-          } else {
-            this.set('showServerError', true);
-          }
-        });
+      this.sendFeedbackTask.perform();
     },
   },
 });
