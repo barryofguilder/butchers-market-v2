1378a5ab090b97ab1da7539a256f7c2cd2dd49f8
Jason Barry <barryofguilder@gmail.com>
2019-10-24 21:24:32 -0400

    Remove mobile app mixin

--- app/pods/deli/route.js
+++ app/pods/deli/route.js
@@ -1,9 +1,8 @@
 import Route from '@ember/routing/route';
 import RSVP from 'rsvp';
 import ResetScrollMixin from 'butchers-market/mixins/reset-scroll-mixin';
-import MobileAppMixin from 'butchers-market/mixins/mobile-app-mixin';
 
-export default Route.extend(ResetScrollMixin, MobileAppMixin, {
+export default Route.extend(ResetScrollMixin, {
   model() {
     return RSVP.hash({
       items: this.store.findAll('deli-item'),
--- app/pods/events/route.js
+++ app/pods/events/route.js
@@ -1,9 +1,8 @@
 import Route from '@ember/routing/route';
 import RSVP from 'rsvp';
 import ResetScrollMixin from 'butchers-market/mixins/reset-scroll-mixin';
-import MobileAppMixin from 'butchers-market/mixins/mobile-app-mixin';
 
-export default Route.extend(ResetScrollMixin, MobileAppMixin, {
+export default Route.extend(ResetScrollMixin, {
   queryParams: {
     events: {
       refreshModel: true,
