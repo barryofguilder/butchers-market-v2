7c86ede96061900e5e66484e6160cbbd39838dc8
Jason Barry <barryofguilder@gmail.com>
2019-12-27 22:30:52 -0500

    Fix error onn main nav for desktop

--- app/pods/components/main-nav/main-nav-items/component.js
+++ app/pods/components/main-nav/main-nav-items/component.js
@@ -1,10 +1,15 @@
 import Component from '@glimmer/component';
 import { inject as service } from '@ember/service';
+import { valueOrDefault } from 'butchers-market/utils/value-or-default';
 
 export default class MainNavItems extends Component {
   @service media;
 
   get isMobile() {
     return !this.media.isLg && !this.media.isXl;
+  }
+
+  get itemClicked() {
+    return valueOrDefault(this.args.itemClicked, () => {});
   }
 }
