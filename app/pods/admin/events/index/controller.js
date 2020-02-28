63d5a72b4a0ebba3a073e6afd12df54fb2ed00f1
Jason Barry <barryofguilder@gmail.com>
2020-02-28 10:47:36 -0500

    Add event pagination and Twitter/OpenGraph meta (#53)
    * Add pagination to events admin
    
    * Add Twitter and Open Graph meta

--- app/pods/admin/events/index/controller.js
+++ app/pods/admin/events/index/controller.js
@@ -3,11 +3,22 @@ import { tracked } from '@glimmer/tracking';
 import { action } from '@ember/object';
 
 export default class AdminEventsIndexController extends Controller {
-  queryParams = ['range'];
+  queryParams = ['page', 'range'];
+  @tracked page = 1;
   @tracked range = 'upcoming';
+
+  get pageMeta() {
+    return this.model.meta.page;
+  }
 
   @action
   rangeUpdated(range) {
     this.range = range;
+    this.page = 1;
+  }
+
+  @action
+  pageChanged(page) {
+    this.page = page;
   }
 }
