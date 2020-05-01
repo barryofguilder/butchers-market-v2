0aed7ff481bd18ee84db1beeddfaaaf418c22e95
Jason Barry <barryofguilder@gmail.com>
2020-05-01 15:53:10 -0400

    Update ember-basic-dropdown

--- app/pods/components/main-nav/main-nav-items/component.js
+++ app/pods/components/main-nav/main-nav-items/component.js
@@ -1,4 +1,5 @@
 import Component from '@glimmer/component';
+import { action } from '@ember/object';
 import { inject as service } from '@ember/service';
 import { valueOrDefault } from 'butchers-market/utils/value-or-default';
 
@@ -11,5 +12,17 @@ export default class MainNavItems extends Component {
 
   get itemClicked() {
     return valueOrDefault(this.args.itemClicked, () => {});
+  }
+
+  @action
+  setDropdown(dropdown) {
+    this.dropdown = dropdown;
+  }
+
+  @action
+  dropdowmMenuClicked() {
+    this.dropdown.actions.close();
+
+    this.itemClicked();
   }
 }
