21931252a14229ef634fa1ce36193eb0bbd9bd92
Jason Barry <barryofguilder@gmail.com>
2021-01-04 20:55:41 -0500

    Remove old validations and tweak Mirage data (#93)
    * Delete old validations
    
    * Tweak the menu mirage data

--- app/validations/performance.js
+++
@@ -1,6 +0,0 @@
-import { validatePresence } from 'ember-changeset-validations/validators';
-
-export default {
-  title: [validatePresence({ presence: true })],
-  link: [validatePresence({ presence: true })],
-};
