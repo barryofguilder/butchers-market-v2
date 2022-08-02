c4c439455f3c5773407b92cf6a77aff8f913c282
Jason Barry <jbarry@hey.com>
2022-08-01 22:06:32 -0400

    Runn prettier on all files

--- .prettierrc.js
+++ .prettierrc.js
@@ -3,4 +3,12 @@
 module.exports = {
   singleQuote: true,
   printWidth: 100,
+  overrides: [
+    {
+      files: 'app/**/*.hbs',
+      options: {
+        singleQuote: false,
+      },
+    },
+  ],
 };
