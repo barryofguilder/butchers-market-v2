1887a53c5348fa509bafd1e280271950ca441a29
Jason Barry <jbarry@hey.com>
2022-08-02 22:41:11 -0400

    Upgrade to Tailwind CSS 3.x

--- ember-cli-build.js
+++ ember-cli-build.js
@@ -1,32 +1,6 @@
 'use strict';
 
 const EmberApp = require('ember-cli/lib/broccoli/ember-app');
-const { join } = require('path');
-const isProduction = EmberApp.env() === 'production';
-
-const purgeCSS = {
-  module: require('@fullhuman/postcss-purgecss'),
-  options: {
-    content: [
-      // Specify all paths in the application that include Tailwind classes.
-      join(__dirname, 'app', 'index.html'),
-      join(__dirname, 'app', 'styles', '**', '*.css'),
-      join(__dirname, 'app', '**', '*.hbs'),
-      join(__dirname, 'app', '**', '*.js'),
-    ],
-
-    // This is the function used to extract class names from your templates
-    defaultExtractor: (content) => {
-      // Capture as liberally as possible, including things like `h-(screen-1.5)`
-      const broadMatches = content.match(/[^<>"'`\s]*[^<>"'`\s:]/g) || [];
-
-      // Capture classes within other delimiters like .block(class="w-1/2") in Pug
-      const innerMatches = content.match(/[^<>"'`\s.()]*[^<>"'`\s.():]/g) || [];
-
-      return broadMatches.concat(innerMatches);
-    },
-  },
-};
 
 module.exports = function (defaults) {
   let app = new EmberApp(defaults, {
@@ -41,17 +15,11 @@ module.exports = function (defaults) {
     postcssOptions: {
       compile: {
         plugins: [
-          {
-            module: require('postcss-import'),
-            options: {
-              path: ['node_modules'],
-            },
-          },
+          require('postcss-import')({ path: ['node_modules'] }),
           require('tailwindcss')('app/tailwind.config.js'),
           require('autoprefixer'),
-          // Only apply the purge CSS plugin to the production builds.
-          ...(isProduction ? [purgeCSS] : []),
         ],
+        cacheInclude: [/.*\.(css|hbs|html|js)$/, /.tailwind\.config\.js$/],
       },
     },
   });
