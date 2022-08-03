1f8fa03df225c1e45f1d291c9587d5bbb7a30b88
Jason Barry <jbarry@hey.com>
2022-08-02 23:53:04 -0400

    Refactor order button

--- app/helpers/order-online-url.js
+++
@@ -1,8 +0,0 @@
-import { helper } from '@ember/component/helper';
-import config from 'butchers-market/config/environment';
-
-export function orderOnlineUrl() {
-  return config.orderOnlineUrl;
-}
-
-export default helper(orderOnlineUrl);
