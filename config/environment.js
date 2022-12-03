c843a25c110dbbfa64238a749df8503684e62d9c
Jason Barry <jbarry@hey.com>
2022-12-03 16:05:08 -0500

    Upgrade to Ember CLI 4.8.0

--- config/environment.js
+++ config/environment.js
@@ -1,7 +1,7 @@
 'use strict';
 
 module.exports = function (environment) {
-  let ENV = {
+  const ENV = {
     modulePrefix: 'butchers-market',
     podModulePrefix: 'butchers-market/pods',
     environment,
