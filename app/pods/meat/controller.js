64f9b27cc21944b225763e41e937929b95363c8b
Jason Barry <jbarry@hey.com>
2023-01-17 13:21:04 -0500

    Update bundle pack PDF link (#111)

--- app/pods/meat/controller.js
+++ app/pods/meat/controller.js
@@ -5,4 +5,16 @@ export default class MeatController extends Controller {
   queryParams = ['packages'];
 
   @tracked packages = false;
+
+  get meatBundlePDF() {
+    const packageBundles = this.model.packageBundles;
+
+    // This is a really hacky way to get the same PDF that we use for the "Mix N' Match" section.
+    let mixnmatch = packageBundles.findBy('title', `Mix N' Match`);
+    if (mixnmatch) {
+      return mixnmatch.fileUrlPath;
+    }
+
+    return '';
+  }
 }
