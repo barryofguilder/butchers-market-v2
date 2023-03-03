543b6877ca1c31a083d65633b386022866674a05
Jason Barry <jbarry@hey.com>
2023-03-03 16:43:43 -0500

    Remove exclude fingerprint

--- ember-cli-build.js
+++ ember-cli-build.js
@@ -4,10 +4,6 @@ const EmberApp = require('ember-cli/lib/broccoli/ember-app');
 
 module.exports = function (defaults) {
   const app = new EmberApp(defaults, {
-    fingerprint: {
-      exclude: ['apple-touch-icon.png', 'images/*.*'],
-    },
-
     'ember-composable-helpers': {
       only: ['sort-by'],
     },
