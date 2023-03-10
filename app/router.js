89365e13636d7baf30a2d84ff1fae3f857609295
Jason Barry <jbarry@hey.com>
2023-03-10 16:57:32 -0500

    Use Google Analytics 4

--- app/router.js
+++ app/router.js
@@ -1,46 +1,15 @@
 import EmberRouter from '@ember/routing/router';
 import config from 'butchers-market/config/environment';
-import { inject as service } from '@ember/service';
 
 export default class Router extends EmberRouter {
   location = config.locationType;
   rootURL = config.rootURL;
-
-  @service metrics;
-  @service router;
-
-  constructor() {
-    super(...arguments);
-
-    let ADMIN_ONLY = false;
-
-    if (document && document.getElementById('admin-only')) {
-      ADMIN_ONLY = true;
-    }
-
-    this.on('routeDidChange', () => {
-      const page = this.router.currentURL;
-      const title = this.router.currentRouteName || 'unknown';
-
-      this.metrics.trackPage({ page, title });
-
-      if (ADMIN_ONLY) {
-        const routeName = this.router.currentRouteName;
-
-        if (!routeName.startsWith('admin') && routeName !== 'down') {
-          this.router.transitionTo('down');
-        }
-      }
-    });
-  }
 }
 
 Router.map(function () {
   this.route('contact');
   this.route('deli');
   this.route('meat');
-
-  this.route('down');
 
   this.route('sign-in');
 
--- ember-cli-build.js
+++ ember-cli-build.js
@@ -1,12 +1,16 @@
 'use strict';
 
 const EmberApp = require('ember-cli/lib/broccoli/ember-app');
+const environment = process.env.EMBER_ENV || 'development';
+const isProduction = environment === 'production';
 
 module.exports = function (defaults) {
   const app = new EmberApp(defaults, {
     'ember-composable-helpers': {
       only: ['sort-by'],
     },
+
+    inlineContent: {},
 
     postcssOptions: {
       compile: {
@@ -21,6 +25,21 @@ module.exports = function (defaults) {
       },
     },
   });
+
+  if (isProduction) {
+    app.options.inlineContent.analytics = {
+      content: `
+        <script async src="https://www.googletagmanager.com/gtag/js?id=G-F0503M6K3V"></script>
+        <script>
+          window.dataLayer = window.dataLayer || [];
+          function gtag(){dataLayer.push(arguments);}
+          gtag('js', new Date());
+
+          gtag('config', 'G-F0503M6K3V');
+        </script>
+      `,
+    };
+  }
 
   const { Webpack } = require('@embroider/webpack');
   return require('@embroider/compat').compatBuild(app, Webpack, {
