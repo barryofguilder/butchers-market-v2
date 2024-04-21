e89a144aa154b1e4cfa274324bf0a1c3f42514e1
Jason Barry <jbarry@hey.com>
2024-04-21 14:55:35 -0400

    Update tailwind config

--- .eslintrc.js
+++ .eslintrc.js
@@ -41,6 +41,7 @@ module.exports = {
         './.stylelintrc.js',
         './.template-lintrc.js',
         './ember-cli-build.js',
+        './postcss.config.js',
         './tailwind.config.js',
         './testem.js',
         './blueprints/*/index.js',
