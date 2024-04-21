7db44536f65ad3a5573a6ef981e6a00cbc8fc180
Jason Barry <jbarry@hey.com>
2024-04-21 15:38:15 -0400

    Use gts templates (#133)

--- app/components/ui-alert.js
+++
@@ -1,8 +0,0 @@
-import Component from '@glimmer/component';
-import { valueOrDefault } from '../utils/value-or-default';
-
-export default class UiAlertComponent extends Component {
-  get variant() {
-    return valueOrDefault(this.args.variant, 'primary');
-  }
-}
