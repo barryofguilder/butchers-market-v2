6b164bce9c80e5f5376f475ff9a365a7569435a8
Jason Barry <jbarry@hey.com>
2025-08-04 16:12:55 -0400

    Upgrade to Ember CLI 6.6.0

--- app/app.ts
+++ app/app.ts
@@ -2,7 +2,12 @@ import Application from '@ember/application';
 import Resolver from 'ember-resolver';
 import loadInitializers from 'ember-load-initializers';
 import config from 'butchers-market/config/environment';
+import { importSync, isDevelopingApp, macroCondition } from '@embroider/macros';
 import 'butchers-market/app.css';
+
+if (macroCondition(isDevelopingApp())) {
+  importSync('./deprecation-workflow');
+}
 
 export default class App extends Application {
   modulePrefix = config.modulePrefix;
