b7b5f2da7bc78ecbb63c50267db076efd4ca3f75
Jason Barry <jbarry@hey.com>
2025-12-19 16:20:43 -0500

    Update modal component (#185)

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,12 +7,34 @@ module.exports = {
       center: true,
     },
     extend: {
+      animation: {
+        'modal-dialog-show': 'fade-in 200ms ease-out, grow-in 200ms ease-out',
+        'modal-overlay-show': 'fade-in 100ms ease-out',
+      },
       colors: {
         overlay: 'rgba(51,51,51,.75)',
         'overlay-light': 'rgba(255,255,255,.3)',
       },
       fontFamily: {
         sans: ['"Source Sans Pro"', ...defaultTheme.fontFamily.sans],
+      },
+      keyframes: {
+        'fade-in': {
+          '0%': {
+            opacity: 0,
+          },
+          '100%': {
+            opacity: 1,
+          },
+        },
+        'grow-in': {
+          '0%': {
+            transform: 'scale(.95)',
+          },
+          '100%': {
+            transform: 'scale(1)',
+          },
+        },
       },
     },
   },
