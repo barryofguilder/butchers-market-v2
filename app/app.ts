3ecb9ed3bdb93fe43f7e21505627da1dba83beb3
Jason Barry <jbarry@hey.com>
2025-12-22 11:03:05 -0500

    Run prettier

--- app/app.ts
+++ app/app.ts
@@ -2,12 +2,12 @@ import Application from '@ember/application';
 import Resolver from 'ember-resolver';
 import loadInitializers from 'ember-load-initializers';
 import { importSync, isDevelopingApp, macroCondition } from '@embroider/macros';
-import config from "./config/environment";
+import config from './config/environment';
 import 'butchers-market/font-awesome';
 import 'butchers-market/app.css';
 import '@warp-drive/ember/install';
 
-import compatModules from "@embroider/virtual/compat-modules";
+import compatModules from '@embroider/virtual/compat-modules';
 
 if (macroCondition(isDevelopingApp())) {
   importSync('./deprecation-workflow');
